4c0ef7989c4e75adc73cb1fe88182743624caa4a
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-04-15 16:48:58 -0500

    Añadidos ejemplos de semana 2

---
+++ weekly_mission_2/examples/Objects_examples/examples0-1.js
@@ -0,0 +1,4 @@
+//ejemplo1: Crear un objeto
+const myObj = {}; //Esto es un objeto vacío
+console.log("Ejemplo 1: Crear un objeto vacío");
+console.log(myObj);
---
+++ weekly_mission_2/examples/Objects_examples/examples0-2.js
@@ -0,0 +1,8 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObject = {
+  name: "Carlo",
+  age: 27,
+};
+
+console.log("Ejemplo 2: Crear un objeto con propiedades");
+console.log(myObject);
