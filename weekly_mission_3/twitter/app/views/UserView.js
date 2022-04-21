62b9657ba85b41462ebe65e5595ad9efa04e5635
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-04-21 14:43:52 -0500

    Paso 2. Válidando llaves de payload

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -1,13 +1,20 @@
-const UserService = require('./../services/UserService')
+const UserService = require("./../services/UserService");
 
-class UserView{
-
-    static createUser(payload){
-        if(payload === null){
-            console.log("Error es null")
-            return {error: "El payload no existe"}
-        }
+class UserView {
+  static createUser(payload) {
+    if (payload === null) {
+      console.log("Error es null");
+      return { error: "El payload no existe" };
+    } else if (
+      typeof payload.username === "string" &&
+      payload.name === "string" &&
+      payload.id === "number"
+    ) {
+      return UserService.create(payload.id, payload.username, payload.name);
+    } else {
+      return { error: "Los datos necesitan tener un valor válido" };
     }
+  }
 }
 
-module.exports = UserView
+module.exports = UserView;
