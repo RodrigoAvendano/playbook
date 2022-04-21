6ce99cf2cddc2c4d2d2c2c3120897ec64f6c99c1
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-04-21 15:54:49 -0500

    Paso 3. Añadida prueba en caso de propiedades faltantes

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -5,11 +5,17 @@ describe("Test for UserView", () => {
     const payload = null;
     const result = UserView.createUser(payload);
     expect(result.error).toMatch(/payload no existe/);
-  });
+  })
 
   test("2. Return an error object when try to create a new user with a payload with invalid properties", () => {
     const payload = { username: null, name: 12, id: "id" };
     const result = UserView.createUser(payload);
     expect(result.error).toMatch(/necesitan tener un valor válido/);
+  })
+
+  test("3. Return an error object when try to create a new user with a payload with missing properties", () => {
+    const payload = { username: "Username" };
+    const result = UserView.createUser(payload);
+    expect(result.error).toMatch(/necesitan tener un valor válido/);
   });
-});
+})
