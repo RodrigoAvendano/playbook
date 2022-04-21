8f126267cf797d651e124a0b5bb677fb1a294180
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-04-21 16:13:39 -0500

    Paso 4. Añadida prueba con datos correctos

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -5,17 +5,26 @@ describe("Test for UserView", () => {
     const payload = null;
     const result = UserView.createUser(payload);
     expect(result.error).toMatch(/payload no existe/);
-  })
+  });
 
   test("2. Return an error object when try to create a new user with a payload with invalid properties", () => {
     const payload = { username: null, name: 12, id: "id" };
     const result = UserView.createUser(payload);
     expect(result.error).toMatch(/necesitan tener un valor válido/);
-  })
+  });
 
   test("3. Return an error object when try to create a new user with a payload with missing properties", () => {
     const payload = { username: "Username" };
     const result = UserView.createUser(payload);
     expect(result.error).toMatch(/necesitan tener un valor válido/);
   });
-})
+
+  test("4. Create an user by a given valid payload", () => {
+    const payload = { username: "username", name: "name", id: 1 };
+    const result = UserView.createUser(payload);
+    console.log(result);
+    expect(result.name).toBe("name");
+    expect(result.username).toBe("username");
+    expect(result.id).toBe(1);
+  });
+});
