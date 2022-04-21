c59313ab000fe9dd9f2db897cf21f63becbdabd3
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-04-20 19:41:02 -0500

    Cambio de carpetas

---
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -0,0 +1,37 @@
+const User = require("./../models/User");
+
+class UserService {
+  static create(id, username, name) {
+    return new User(id, username, name, "My bio");
+  }
+
+  static getInfo(user) {
+    let list = [];
+    Object.keys(user).map((key) => {
+      list.push(user[key]);
+    });
+    return list;
+  }
+
+  static updateUsername(user, newUserName) {
+    //  const myObj = new User(user.id, user.username, user.name, 'My bio');
+    //   myObj.setUsername = newUserName
+    //   console.log()
+    const myUser = user;
+  }
+
+  static getAllUsernames(names) {
+    // let array = []
+    // names.forEach(element => {
+    //   array.push(element.username);
+    // });
+    // return array
+
+    const array = names.map((element) => {
+      return element.username;
+    });
+    return array;
+  }
+}
+
+module.exports = UserService;
