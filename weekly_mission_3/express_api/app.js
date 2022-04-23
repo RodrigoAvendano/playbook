6b235953f549042ecce8eda628e15a1490ad533f
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-04-23 17:38:45 -0500

    Endpoint que crea un explorer con POST

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -17,6 +17,21 @@ app.get("/v1/explorers", (req, res) => {
   res.status(200).json(explorers);
 });
 
+// GET Crea un endpoint que te regrese un explorer mediando un ID
+app.get("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers GET request ${new Date()}`);
+  console.log(`Getting explorer with id ${req.params.id}`);
+  const explorer = { id: 1, name: "Carlo" };
+  res.status(200).json(explorer);
+});
+
+// POST Crea un endpoint que se encargue de crear un explorer
+app.get("/v1/explorers", (req, res) => {
+  console.log(`Api Explorers POST request ${new Date()}`);
+  const requestBody = req.body; //Parámetros de un ciente
+  res.status(201).json({ message: "Created" });
+});
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
