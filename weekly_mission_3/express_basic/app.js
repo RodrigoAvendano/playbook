fce050552a5c7699cbb50670248edec87d7943de
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-04-22 21:04:11 -0500

    Agregando una ruta que reciba Query Params

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -1,29 +1,37 @@
-//Usando objeto express
+// Usando objeto express
 const express = require("express");
-//App de Express
+// App de Express
 const app = express();
-//Puerto en el que vamos a ver nuestra app: localhost:3000
+// Puerto en el que vamos a ver nuestra app: localhost:3000
 const port = 3000;
 
-//path inicial, responderá la urllocalhost:3000
+// path inicial, responderá la urllocalhost:3000
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-//Respondiendo texto
-//localhost:3000/launchx
+// Respondiendo texto
+// localhost:3000/launchx
 app.get("/launchx", (req, res) => {
   res.send("Bienvenidos a LaunchX");
 });
 
-//Regresando un objeto
-//localhost:3000/explorersInNode
+// Regresando un objeto
+// localhost:3000/explorersInNode
 app.get("/explorersInNode", (req, res) => {
   const explorer = { name: "Explorer", msg: "Hello" };
   res.send(explorer);
 });
 
-//Con esto inicializamos la app
+// Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/carlo
+// req.params = {explorerName : "carlo"}
+
+app.get("/explorers/:explorerName", (req, res) => {
+  res.send(req.params);
+});
+
+// Con esto inicializamos la app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
