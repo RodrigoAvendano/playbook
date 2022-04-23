84456b9f1ae766d8dd833c1cc15dfe43a2cb3e83
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-04-22 20:02:29 -0500

    Regresando un objeto con express

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -1,22 +1,29 @@
 //Usando objeto express
-const express = require('express')
+const express = require("express");
 //App de Express
-const app = express()
+const app = express();
 //Puerto en el que vamos a ver nuestra app: localhost:3000
-const port = 3000
+const port = 3000;
 
 //path inicial, responderá la urllocalhost:3000
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //Respondiendo texto
 //localhost:3000/launchx
-app.get('/launchx', (req, res) => {
-    res.send('Bienvenidos a LaunchX')
-})
+app.get("/launchx", (req, res) => {
+  res.send("Bienvenidos a LaunchX");
+});
+
+//Regresando un objeto
+//localhost:3000/explorersInNode
+app.get("/explorersInNode", (req, res) => {
+  const explorer = { name: "Explorer", msg: "Hello" };
+  res.send(explorer);
+});
 
 //Con esto inicializamos la app
-app.listen(port, () =>{
-    console.log(`Example applistening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
